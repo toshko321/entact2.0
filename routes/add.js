ee2cd40f6b38655b866a8158df5b2032de833fba
Teodor <teodorppetrov@gmail.com>
2017-03-08 09:20:40 -0800

    gotta make create form look good and add buttons on browse page

--- routes/add.js
+++ routes/add.js
@@ -1,18 +1,31 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
-	// Add new friend's data to data object
-	var newName = req.query.name;
-	var newDescription = req.query.description;
-	var newFriend = {
-		"name": newName, 
-		"description": newDescription,
-		"imageURL": "http://lorempixel.com/400/400/people"
-	};
+    // Your code goes here
+    // Add new friend's data to data object
+    var newName = req.query.name;
+    var newCreator = req.query.creator;
+    var newDescription = req.query.description;
+    var newLocation = req.query.location;
+    var newTime = req.query.time;
+    var newItems = req.query.items;
+    var newMembers = req.query.members;
+    var newDate = req.query.date;
+    
+    var newFriend = {
+        "name": newName, 
+        "description": newDescription,
+        "creator": newCreator,
+        "location": newLocation,
+        "time": newTime,
+        "items": newItems,
+        "members": newMembers,
+        "date": newDate
+        
+    };
 
-	data.friends.push(newFriend);
-	console.log(data);
-	res.render('add', data);
-	// Render index.handlebars
+    data.friends.unshift(newFriend);
+    console.log(data);
+    res.render('add', data);
+    // Render index.handlebars
  }
