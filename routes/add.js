eff7a3d9abc57898ce70411ea82824a6b7cf02b8
Teodor <teodorppetrov@gmail.com>
2017-03-08 22:49:31 -0800

    Date and time formatted

--- routes/add.js
+++ routes/add.js
@@ -1,4 +1,5 @@
 var data = require("../data.json");
+var d3 = require("d3");
 
 exports.addEvent = function(req, res) {    
     // Your code goes here
@@ -11,16 +12,23 @@ exports.addEvent = function(req, res) { 
     var newItems = req.query.items;
     var newMembers = req.query.members;
     var newDate = req.query.date;
+    //var formattedDate = formatDate(newDate);
+
+    //var dateLast = "2016-02-18";
+    var format = d3.timeFormat("%B %d, %Y");
+    var formattedDate = format(new Date(newDate));
+
+    var formattedTime = formatTime(newTime);
     
     var newEvent = {
         "name": newName, 
         "description": newDescription,
         "creator": newCreator,
         "location": newLocation,
-        "time": newTime,
+        "time": formattedTime,
         "items": newItems,
         "members": newMembers,
-        "date": newDate
+        "date": formattedDate
         
     };
 
@@ -29,3 +37,15 @@ exports.addEvent = function(req, res) { 
     res.render('add', data);
     // Render index.handlebars
  }
+
+function formatTime(time24) 
+{
+  var ts = time24;
+  var H = +ts.substr(0, 2);
+  var h = (H % 12) || 12;
+  h = (h < 10)?(h):h;  // leading 0 at the left for 1 digit hours
+  var ampm = H < 12 ? " AM" : " PM";
+  ts = h + ts.substr(2, 3) + ampm;
+  return ts;
+};
+
